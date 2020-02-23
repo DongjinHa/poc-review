8e0e9942cc25ce74b91481a1f8b6d86dac915024
DongjinHa <33052838+DongjinHa@users.noreply.github.com>
2020-02-23 23:29:33 +0900

    상품조회 API 상품명 조회기능 추가

--- poc-product-api/src/main/java/com/example/demo/ProductService.java
+++ poc-product-api/src/main/java/com/example/demo/ProductService.java
@@ -1,7 +1,12 @@
 package com.example.demo;
 
+import java.util.List;
+
 import com.example.demo.ProductDTO;
 
 public interface ProductService {
+	
 	public ProductDTO getProductListByPrdSeq(String prdSeq);
+	public List<ProductDTO> getProductListByPrdNm(String prdNm);
+
 }
