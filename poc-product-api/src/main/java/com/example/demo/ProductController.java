024a2e7093e0681781d67f11266e4e588747690d
DongjinHa <33052838+DongjinHa@users.noreply.github.com>
2020-02-23 23:29:33 +0900

    상품조회 API 상품명 조회기능 추가

--- poc-product-api/src/main/java/com/example/demo/ProductController.java
+++ poc-product-api/src/main/java/com/example/demo/ProductController.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,4 +18,10 @@ public class ProductController {
     public ProductDTO getProductListByPrdSeq(@PathVariable String prdSeq) {	
         return productService.getProductListByPrdSeq(prdSeq);
     }
+	
+	@GetMapping("/getProductListByPrdNm/{prdNm}")
+    public List<ProductDTO> getProductListByPrdNm(@PathVariable String prdNm) {	
+        return productService.getProductListByPrdNm(prdNm);
+    }
+	
 }
--- poc-product-api/src/main/java/com/example/demo/ProductServiceImpl.java
+++ poc-product-api/src/main/java/com/example/demo/ProductServiceImpl.java
@@ -3,7 +3,16 @@ package com.example.demo;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.AggregationResults;
+import org.springframework.data.mongodb.core.aggregation.LimitOperation;
+import org.springframework.data.mongodb.core.aggregation.LookupOperation;
+import org.springframework.data.mongodb.core.aggregation.MatchOperation;
+import org.springframework.data.mongodb.core.aggregation.ProjectionOperation;
+import org.springframework.data.mongodb.core.aggregation.SkipOperation;
+import org.springframework.data.mongodb.core.aggregation.SortOperation;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
@@ -15,7 +24,6 @@ import com.example.demo.ProductRepository;
 public class ProductServiceImpl implements ProductService {
 
 	@Autowired
-	private ProductRepository productRepository;
 	private final MongoTemplate mongoTemplate;
 	
 	public ProductServiceImpl(MongoTemplate mongoTemplate) {
@@ -27,6 +35,12 @@ public class ProductServiceImpl implements ProductService {
 				.addCriteria(Criteria.where("prdSeq").is(prdSeq));
 		ProductDTO product = mongoTemplate.findOne(query, ProductDTO.class,"products");
 		return product;
+	}
+	
+	public List<ProductDTO> getProductListByPrdNm(String prdNm) {
+		Query query = new Query()
+				.addCriteria(Criteria.where("prdNm").regex(prdNm));
+		return mongoTemplate.find(query, ProductDTO.class);
 	}
 	
 }
