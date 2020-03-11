273c3d6b52b1efe7f5bd2135d6451d5efc9c02d3
DongjinHa <33052838+DongjinHa@users.noreply.github.com>
2020-03-11 23:03:13 +0900

    remove project

--- bk/poc-product-api/src/main/java/com/example/demo/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.demo.ProductDTO;
-
-public interface ProductRepository extends MongoRepository<ProductDTO, String> {
-
-}
--- bk/poc-review-api/src/main/java/com/msa/repository/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.msa.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.msa.document.Comment;
-
-public interface CommentRepository extends MongoRepository<Comment, String> {
-	
-}
