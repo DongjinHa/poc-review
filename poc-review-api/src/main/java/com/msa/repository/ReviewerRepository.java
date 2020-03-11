d6b5008813ec4037ebafefb83de8d2c442df2667
DongjinHa <33052838+DongjinHa@users.noreply.github.com>
2020-03-11 23:30:58 +0900

    remove project

--- poc-review-api/src/main/java/com/msa/repository/ReviewerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.msa.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.msa.document.Reviewer;
-
-public interface ReviewerRepository extends MongoRepository<Reviewer, String> {
-	
-}
