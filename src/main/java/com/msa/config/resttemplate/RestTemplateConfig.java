b53f5aac2112274de0125a74db73b6959ca04b6b
DongjinHa <33052838+DongjinHa@users.noreply.github.com>
2020-03-27 21:17:20 +0900

    delete interceptors code

--- src/main/java/com/msa/config/resttemplate/RestTemplateConfig.java
+++ src/main/java/com/msa/config/resttemplate/RestTemplateConfig.java
@@ -18,7 +18,7 @@ public class RestTemplateConfig {
 	@Bean
 	public RestTemplate reviewTemplate(@Value("${int.review-api.url}") String reviewAPI) {
 		return restTemplateBuilder.rootUri(reviewAPI)
-	        .additionalInterceptors(new RestTemplateClientHttpRequestInterceptor())
+//	        .additionalInterceptors(new RestTemplateClientHttpRequestInterceptor())
 	        .errorHandler(new RestTemplateErrorHandler())
 	        .setConnectTimeout(Duration.ofMinutes(3))
 	        .build();
@@ -27,7 +27,7 @@ public class RestTemplateConfig {
 	@Bean
 	public RestTemplate productTemplate(@Value("${int.product-api.url}") String productAPI) {
 		return restTemplateBuilder.rootUri(productAPI)
-	        .additionalInterceptors(new RestTemplateClientHttpRequestInterceptor())
+//	        .additionalInterceptors(new RestTemplateClientHttpRequestInterceptor())
 	        .errorHandler(new RestTemplateErrorHandler())
 	        .setConnectTimeout(Duration.ofMinutes(3))
 	        .build();
